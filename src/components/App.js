99832d58dacde6242ed713ec30ba1d05782de864
Ajay70aa <ajay70aa@gmail.com>
2025-05-21 11:23:56 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,55 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [fields, setFields] = useState([{ name: '', age: '' }])
+  function addField(){
+    setFields([...fields, { name: "", age: ""}])
+  }
+  function handleChange(e, index){
+    const updatedFields = [...fields]
+    updatedFields[index][e.target.name] = e.target.value
+    setFields(updatedFields)
+  }
+  function handleSubmit(e){
+    e.preventDefault()
+    console.log(fields)
+  }
+  function removeField(index){
+    const updatedFields = fields.filter((_, i) => i !== index)
+    setFields(updatedFields)
+  }
   return (
-    <div>
+    <div className="form-container">
         {/* Do not remove the main div */}
+        <form onSubmit={handleSubmit}>
+          {fields.map((field, index) => {
+            return (
+              <div key={index} className="fields-container">
+                <input 
+                  type="text" 
+                  placeholder="name" 
+                  name="name" 
+                  value={field.name}
+                  onChange={(e) => handleChange(e, index)}
+                />
+                <input 
+                  type="number" 
+                  placeholder="age" 
+                  name="age" 
+                  value={field.age} 
+                  onChange={(e) => handleChange(e, index)}
+                />
+                <button type="button" onClick={() => removeField(index)}>Remove</button>
+              </div>
+            )})
+            }
+            <div className="buttons-container">
+              <button type="button" onClick={addField}>Add More</button>
+              <button type="submit">Submit</button>
+            </div>
+        </form>
     </div>
   )
 }
